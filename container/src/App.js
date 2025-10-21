af28ff1449101d94db51aac8a045a10be4da45ff
Piotr <garlej.p@gmail.com>
2025-10-21 21:10:28 +0200

    Update

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hello, Container!</h1>
+      <h1>Hello, My Container!</h1>
       <hr />
       <MarketingApp />
     </div>
